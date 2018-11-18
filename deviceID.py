611bca9039ad902895cf274c6ec743d2ccbe69a4
Ludwig Possie <ludwigpossie@weber.edu>
2018-11-18 16:21:56 -0700

    Correction to multiple argument syntax.

--- deviceID.py
+++ deviceID.py
@@ -2,7 +2,7 @@ import argparse
 
 parser=argparse.ArgumentParser(description="Plex media player ID script.")
 
-parser.add_argument('id', nargs = +)
+parser.add_argument('id', nargs = '+')
 
 args = parser.parse_args()
 
